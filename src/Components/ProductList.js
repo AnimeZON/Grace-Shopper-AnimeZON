ddd617668af257dcb833f5930f92f4df072866bb
Sean <seanbennettdolan@gmail.com>
2023-06-06 17:11:58 -0400

    ProductList outline part 2

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -2,22 +2,23 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 const ProductList = ()=> {
-  const {  } = useSelector(state => state);
-  const dispatch = useDispatch();
+  const { products } = useSelector(state => state);
+  // const dispatch = useDispatch();
   return (
     <div className="products">
       <h1>Products</h1>
       {products.map((product) => {
-        product
+        <div>
+        <img 
+        src={product.image}
+        alt={product.name}
+        style={{ width: "50%", height: "auto" }}
+        />
+        <p>Item Name: {product.name} </p>
+        <p>Item Price {product.price} </p>
+        <button>Add to Cart</button>
+        </div>
 })}
-<img 
-src={product.image}
-alt={product.name}
-style={{ width: "50%", height: "auto" }}
-/>
-<p>Item Name: {product.name} </p>
-<p>Item Price {product.price} </p>
-<button>Add to Cart</button>
     </div>
   );
 };
