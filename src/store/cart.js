9f3e4d7e0fda2062be9177ad5c170b179c3faaef
Phillip Phifer <38228979+Minsu386@users.noreply.github.com>
2023-06-08 11:53:09 -0400

    updated cartSlice with remove and add items cases.

--- src/store/cart.js
+++ src/store/cart.js
@@ -19,12 +19,48 @@ export const fetchCart = createAsyncThunk("fetchCart", async()=>{
   }
 })
 
+export const removeItem = createAsyncThunk("removeItem", async(removeItems)=>{
+  try{
+    const token = window.localStorage.getItem('token');
+    const {data} = await axios.put('/api/orders/cart', removeItems, {
+      headers: {
+        authorization: token
+      }
+    });
+    return data;
+  }catch(er){
+    console.log(er);
+  }
+})
+
+export const addItem = createAsyncThunk("addItem", async(addItem)=>{
+  console.log(addItem)
+  try{
+    const token = window.localStorage.getItem('token');
+    const {data} = await axios.post('/api/orders/cart', addItem, {
+      headers:{
+        authorization: token
+      }
+    });
+    return data;
+  }catch(er){
+    console.log(er);
+  }
+})
+
+
 const cartSlice = createSlice({
   name:"cart",
   initialState,
   reducers: {},
   extraReducers: (builder)=>{
     builder.addCase(fetchCart.fulfilled, (state, action)=>{
+      return action.payload;
+    }),
+    builder.addCase(removeItem.fulfilled, (state, action) => {
+      return action.payload;
+    }),
+    builder.addCase(addItem.fulfilled, (state, action ) => {
       return action.payload;
     })
   }
