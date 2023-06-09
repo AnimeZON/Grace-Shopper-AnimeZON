8a72061944c17ea5942156796fc64c4e80eeff3a
ellie <flowerskullxoxo@gmail.com>
2023-06-09 09:44:34 -0700

    cart page set up + add to cart button on singleProduct

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchCart, addItem, removeItem } from '../store/cart';
 import { logout } from '../store';
 import { Link } from 'react-router-dom';
 
 const Cart = ()=> {
-  const { cart } = useSelector(state => state);
   const dispatch = useDispatch();
+  const { cart } = useSelector(state => state);
+  const { auth } = useSelector(state => state);
+
+  useEffect(() => {
+    dispatch(fetchCart())
+  }, []);
+
   return (
     <div>
       <h1>Cart</h1>
-      <pre>
-        {
-          JSON.stringify(cart, null, 2)
-        }
-      </pre>
+      <div>
+        {cart.lineItems.map((lineItem) => {
+          return (
+            <div>
+              <img
+                src={lineItem.product.image}
+                alt={lineItem.product.name}
+                style={{ width: "20%", height: "auto" }}
+              />
+              <Link to={`/product/${lineItem.product.id}`}> {lineItem.product.name} </Link>
+               {lineItem.product.price} 
+            </div>
+          )
+          })}
+        </div> 
     </div>
   );
 };
